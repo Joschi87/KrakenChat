96c3103b5db4daadcc6768f710349d96c79f15ff
Janosch Walter <janosch.walter27@gmail.com>
2019-03-16 00:50:37 +0100

    Add the import of the chat commands and create the file for the chat commands

---
+++ KrakenChat/terminal/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+import connection
+import chat_commands
+
---
+++ KrakenChat/terminal/chat_commands.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+#/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
