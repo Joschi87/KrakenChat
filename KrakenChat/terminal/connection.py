bc5cdefc1effa424dcc08573f502a5d0996b228d
Janosch Walter <janosch.walter27@gmail.com>
2019-03-15 22:53:00 +0100

    Start to code a terminal version of the chat

---
+++ KrakenChat/terminal/connection.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+import mysql.connector
+
+verbindung = mysql.connector.connect(
+	host="localhost",
+	user="admin",
+	passwd="passwort"
+	)
