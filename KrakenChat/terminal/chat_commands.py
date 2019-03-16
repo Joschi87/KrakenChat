f8e5dd86ad0c271796bfa27dd86a9a4270248e55
Janosch Walter <janosch.walter27@gmail.com>
2019-03-16 01:51:13 +0100

    Start to write the class of chat commands

--- KrakenChat/terminal/chat_commands.py
+++ KrakenChat/terminal/chat_commands.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 #/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+
+class ChatCommands():
+
+	def __init__(self):
+
+		#login member variables
+
+		self.username = "";
+		self.passwd = "";
+
+		#ready-made chat commands member variable
+
+		self.selectDB = "ls -all:/";
+		self.selectChat = "ls -chat:/";
+		self.showUser = "ls -users:/";
+		self.backstep = "cd -back:/";
+		self.forward = "cd -forward:/";
+		self.writeMessage = "echo -message:/";
