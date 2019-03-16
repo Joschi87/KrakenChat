1374549ac229c7c7dd6f7184e9861cb69ec48722
Janosch Walter <janosch.walter27@gmail.com>
2019-03-16 22:16:21 +0100

    Start to working at the app.py and use the mac osx shebang in all files

--- KrakenChat/terminal/app.py
+++ KrakenChat/terminal/app.py
@@ -1,7 +1,12 @@
-#!/usr/bin/python3
-#/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+#/usr/bin/python3
 
 #import connection
+import getpass
 from chatcommands import ChatCommands
 
-chatcommands = ChatCommands()
+#chatcommands = ChatCommands()
+
+username = input("Username: ")
+password = getpass.getpass("Password: ")
+
