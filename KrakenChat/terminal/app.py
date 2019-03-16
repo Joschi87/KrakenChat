ad01f61c40e775530911fff013f316e42c8fedc9
Janosch Walter <janosch.walter27@gmail.com>
2019-03-16 21:19:02 +0100

    fixed bug and test the function needhelp()

--- KrakenChat/terminal/app.py
+++ KrakenChat/terminal/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 #/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
 
-import connection
-import chat_commands
+#import connection
+from chatcommands import ChatCommands
 
+chatcommands = ChatCommands()
