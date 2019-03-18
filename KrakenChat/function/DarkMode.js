29d663774513d61914c404c8fbb22691ce861752
Janosch Walter <janosch.walter27@gmail.com>
2019-03-18 16:29:38 +0100

    Delete the old structur

--- KrakenChat/function/DarkMode.js
+++
@@ -1,7 +0,0 @@
-
-//Set the normal color to the DarkMode colors
-
-document.getElementByID("ChatWindow").style.color = "black";
-document.getElementByID("creator").style.color = "green";
-document.getElementByID("message").style.color = "green";
-document.getElementByID("DateTime").style.color = "green";
--- KrakenChat/function/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $verbindung = mysqli_connect("", "", "", "")
-                      or die("Keine Verbindung zum Server");
-    //require_once("report.php");
-    
-?>
