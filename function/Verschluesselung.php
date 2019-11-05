2c7949b8e0ecdcff79872769607cff860611a105
Janosch Walter <janosch.walter27@gmail.com>
2019-11-05 20:11:03 +0100

    Working at the adaptation for the loadChats Class

--- function/Verschluesselung.php
+++ function/Verschluesselung.php
@@ -6,17 +6,19 @@
 
 	//Chat ID runterladen aus dem Header
 
-	$Chat_ID = $_GET["ChatID"];
+	$chat_ID = $_GET["ChatID"];
 
 	//Key aus der ChatConnection runterladen
 
-	$download_key = mysqli_query($verbindung, "SELECT VSK FROM ChatConnection WHERE ChatID = '$Chat_ID'");
+	$download_key = mysqli_query($verbindung, "SELECT VSK FROM ChatConnection WHERE ChatID = '$chat_ID'");
 
 	while($row = mysqli_fetch_assoc($download_key)){
 
 		$key = $row['VSK'];
 
 	}
+
+	mysqli_close($verbindung);
 
 	//Blowfish Class runterladen
 
@@ -27,6 +29,6 @@
 	$blowfish = new Blowfish($key);
     $ausgabe_2 = $blowfish->Encrypt($text);
 
-    $GLOBALS['ausgabe'] = $ausgabe_2;
-
+	$GLOBALS['ausgabe'] = $ausgabe_2;
+	
 ?>
